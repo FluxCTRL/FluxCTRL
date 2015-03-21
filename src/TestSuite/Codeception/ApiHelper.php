9dcff25d04c8b4b0d15d040459ebcff56d6587c4
Jad Bitar <jadbitar@mac.com>
2015-03-21 17:06:59 -0400

    Add `TestSuite\ApiHelper`

---
+++ src/TestSuite/Codeception/ApiHelper.php
@@ -0,0 +1,15 @@
+<?php
+namespace FluxCtrl\TestSuite\Codeception;
+
+use Codeception\Module;
+use Codeception\TestCase as Test;
+
+class ApiHelper extends Module
+{
+
+    public function _before(Test $test)
+    {
+        $this->getModule('Cake\Codeception\Helper')
+            ->loadFixtures('app.feeds', 'app.items');
+    }
+}
